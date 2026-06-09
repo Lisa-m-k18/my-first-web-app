8478feabaef0982abd23028c0c8797c7cc2a2d44
Lisa-k18 <mochi.lisa.mangetsu@gmail.com>
2026-06-09 14:15:07 +0900

    Update flashcards.js

--- js/flashcards.js
+++ js/flashcards.js
@@ -1,6 +1,39 @@
-// サーバーからデータを取得する関数を作成してください
-
+async function fetchFlashcards() {
+  try {
+    const response = await fetch("/api/flashcards");
+    return response.json();
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
 export async function setupFlashcards() {
   // 暗記カード機能に必要な処理を作成してください
+  const flashcardsList = getElementById("flashcards-list");
 
+  async function LeadFlashcards() {
+    const wordList = await fetchFlashcards();
+    renderFlashcards(wordList);
+  }
+  async function renderFlashcards(wordList) {
+    flashcardsList.innerHTML = "";
+    wordList.forEach((word) => {
+      const flashcard = `
+      <div class="flashcard">
+        <div class="flashcard-content">
+          <p class="flashcard-title">${word.word}</p>
+          <div class="flashcard-icons">
+            <button data-toggel="${word.id}" class="flashcard-meaning">
+              <span class="ri-eye-line"></span>
+            <button>
+          </div>
+        </div>
+        <div data-meaning="${word.id}" class="hidden">
+          <p class="flashcard-toggle">${word.meaning}</p>
+        </div>
+      </div>
+      `;
+      flashcardsList.innerHTML += flashcard;
+    });
+  }
 }
